fbeeeccb9d973d2b2c99af04df6870db23a23cf8
Takahito Nakano <kano@ponko2.jp>
2016-07-15 01:50:30 +0900

    Laravel Elixir v6.0.0-9 への対応

---
+++ src/tasks/StylelintTask.js
@@ -0,0 +1,67 @@
+const objectAssign = require('object-assign');
+
+let stylelint;
+
+export default class StylelintTask extends Elixir.Task {
+  /**
+   * Create a new StylelintTask instance.
+   *
+   * @param {string}      name    Task name
+   * @param {GulpPaths}   paths   Gulp src and output paths
+   * @param {object|null} options Stylelint options
+   */
+  constructor(name, paths, options) {
+    super(name, null, paths);
+
+    this.output  = undefined;
+    this.options = options || {};
+  }
+
+  /**
+   * Lazy load the task dependencies.
+   *
+   * @returns {void}
+   */
+  loadDependencies() {
+    // eslint-disable-next-line global-require
+    stylelint = require('gulp-stylelint');
+  }
+
+  /**
+   * Build up the Gulp task.
+   *
+   * @returns {void}
+   */
+  gulpTask() {
+    return gulp.src(this.src.path)
+      .pipe((() => {
+        this.recordStep(`Executing ${this.ucName()}`);
+        return this.lint();
+      })())
+      .on('error', this.onError());
+  }
+
+  /**
+   * Register file watchers.
+   *
+   * @returns {void}
+   */
+  registerWatchers() {
+    this.watch(this.src.path);
+  }
+
+  /**
+   * Lint task
+   *
+   * @returns {Stream} Object stream usable in Gulp pipes.
+   */
+  lint() {
+    return stylelint(objectAssign({
+      failAfterError: true,
+      reporters: [{
+        formatter: 'string',
+        console: true
+      }]
+    }, this.options));
+  }
+}
---
+++ src/tasks/recipes/stylelint.js
@@ -0,0 +1,30 @@
+import StylelintTask from '../StylelintTask';
+
+/**
+ * Stylelint Task
+ *
+ * @param {string|Array|null} src     Glob or array of globs
+ * @param {object|null}       options Stylelint options
+ * @returns {void}
+ */
+function task(src, options) {
+  // eslint-disable-next-line no-new
+  new StylelintTask('stylelint', getPaths(src), options);
+}
+
+/**
+ * Prep the Gulp src paths.
+ *
+ * @param {string|Array|null} src Glob or array of globs
+ * @returns {GulpPaths} Gulp src paths
+ */
+function getPaths(src) {
+  return new Elixir.GulpPaths()
+    .src(src || [
+      `${Elixir.config.get('assets.css.folder')}/**/*.css`,
+      `${Elixir.config.get('public.css.outputFolder')}/**/*.css`,
+      `!${Elixir.config.get('public.css.outputFolder')}/vendor/**/*.css`
+    ]);
+}
+
+Elixir.extend('stylelint', task);
